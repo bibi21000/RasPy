015ebbba43319358fc3288216ea5f1d99dfd72b0
bibi21000 <bibi21000@gmail.com>
2015-01-13 00:28:36 +0100

    Merge client in mdcliapi

--- src/raspy/common/mdcliapi.py
+++ src/raspy/common/mdcliapi.py
@@ -16,7 +16,7 @@ import raspy.common.MDP as MDP
 class MajorDomoClient(object):
     """Majordomo Protocol Client API, Python version.
 
-      Implements the MDP/Worker spec at http:#rfc.zeromq.org/spec:7.
+      Credits : https://github.com/imatix/zguide/blob/master/examples/Python/mdcliapi.py
     """
     broker = None
     ctx = None
@@ -81,6 +81,112 @@ class MajorDomoClient(object):
                     break
                 retries -= 1
         return reply
+
+    def destroy(self):
+        """ Destroy object
+        """
+        self.ctx.destroy(0)
+
+class TitanicClient(Executive):
+    """The titanic client
+
+    Credits: https://github.com/imatix/zguide/blob/master/examples/Python/ticlient.py
+
+    """
+
+    def __init__(self, hostname='localhost', service="worker", broker_ip='127.0.0.1', broker_port=5514, poll=1500, ttl=900):
+        """Initialize the client
+        """
+        Executive.__init__(self, hostname, service, broker_ip, broker_port)
+        self.client = MajorDomoClient("tcp://%s:%s" % (broker_ip, broker_port))
+        self.poll = poll
+        self.ttl = ttl
+        self.requests = {}
+
+    def request(self, service=None, data=["mmi.echo"], callback=None, args=(), kwargs={}):
+        """Request a job for a worker to titanic
+        """
+        req = [MDP.routing_key(self.hostname, service)] + data if service else [MDP.routing_key(self.hostname, self.service)] + data
+        reply = self.client.send("titanic.request", req)
+        uuid = None
+        if reply:
+            status = reply.pop(0)
+            if status == MDP.T_OK:
+                uuid = reply.pop(0)
+                self.requests[uuid] = { \
+                        'callback' : callback, \
+                        'args' : args, \
+                        'kwargs' : kwargs, \
+                        'status' : MDP.T_PENDING, \
+                        'data' : None, \
+                        'req_date' : time.time(), \
+                        }
+                logging.debug("CLIENT - Request UUID %s", uuid)
+            elif status == MDP.T_UNKNOWN:
+                logging.error("CLIENT - MDP.ClientError in titanic.request : routing_key=%s data=%s", req, data)
+                raise MDP.ClientError("titanic.request : routing_key=%s data=%s" % (req, data))
+            elif status == MDP.T_ERROR:
+                logging.error("CLIENT - MDP.ServerError in titanic.request : routing_key=%s data=%s", req, data)
+                raise MDP.ServerError("titanic.request : routing_key=%s data=%s" % (req, data))
+        return uuid
+
+    def status(self, uuid):
+        """Retrieve the status of a work from titanic
+        """
+        if uuid:
+            if uuid in self.requests:
+                status = self.requests[uuid]['status']
+                if status != MDP.T_PENDING:
+                    #The tasks is finished or is in error. Remove it from pending requests
+                    del self.requests[uuid]
+                return status
+        return MDP.T_UNKNOWN
+
+    def run(self):
+        """Run the client in a loop
+        """
+        while not self._stopevent.isSet():
+            start_at = time.clock()
+            uuids = [uid for uid in self.requests.keys() if self.requests[uid]['status'] == MDP.T_PENDING]
+            for uuid in uuids:
+                reply = self.client.send("titanic.reply", [uuid])
+                if reply:
+                    status = reply.pop(0)
+                    self.requests[uuid]['status'] = status
+                    if status == MDP.T_OK:
+                        self.requests[uuid]['data'] = reply
+                        if self.requests[uuid]['callback']:
+                            self.requests[uuid]['callback'](reply, self.requests[uuid]['args'], self.requests[uuid]['kwargs'])
+                            del self.requests[uuid]
+                        logging.debug("CLIENT - Titanic received: %s", reply)
+                        reply = self.client.send("titanic.close", [uuid])
+                        if reply:
+                            status = reply.pop(0)
+                            if status == MDP.T_UNKNOWN:
+                                logging.error("CLIENT - MDP.ClientError in titanic.close : uuid=%s", uuid)
+                                raise MDP.ClientError("titanic.close : uuid=%s" % (uuid))
+                            elif status == MDP.T_ERROR:
+                                logging.error("CLIENT - MDP.ServerError in titanic.close : uuid=%s", uuid)
+                                raise MDP.ServerError("titanic.close : uuid=%s" % (uuid))
+                    elif status == MDP.T_UNKNOWN:
+                        logging.error("CLIENT - MDP.ClientError in titanic.reply : uuid=%s", uuid)
+                        raise MDP.ClientError("titanic.reply : uuid=%s" %(uuid))
+                    elif status == MDP.T_ERROR:
+                        logging.error("CLIENT - MDP.ServerError in titanic.reply : uuid=%s", uuid)
+                        raise MDP.ServerError("titanic.reply : uuid=%s" %(uuid))
+            now = time.time()
+            uuids_to_del = [uid for uid in self.requests.keys() if (self.requests[uid]['status'] == MDP.T_UNKNOWN or \
+                                                                     self.requests[uid]['status'] == MDP.T_ERROR or \
+                                                                     self.requests[uid]['status'] == MDP.T_ERROR) and \
+                                                                     (now-self.requests[uid]['req_date']) > self.ttl]
+            for uuid in uuids_to_del:
+                logging.info("CLIENT - Titanic remove uuid %s because of ttl", uuid)
+                del self.requests[uuid]
+            sleep_time = self.poll - (time.clock() - start_at)
+            if sleep_time > 0:
+                self._stopevent.wait(sleep_time/500.0)
+            else:
+                logging.warning("CLIENT - Not enough time to sleep with current poll : %s ", sleep_time)
 
     def destroy(self):
         """ Destroy object
